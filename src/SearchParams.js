ca7c8dcc1a6ef66485fe7eabb721d79ff94829e8
corozb <cristianorozco1984@gmail.com>
2020-03-27 10:18:54 -0500

    State with Hooks

---
+++ src/SearchParams.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const SearchParams = () => {
+	const [location, setLocation] = useState('Seatle, WA');
+
+	return (
+		<div className='search-params'>
+			<h1>{location}</h1>
+			<form>
+				<label htmlFor='location'>
+					location
+					<input
+						type='text'
+						id='location'
+						value={location}
+						placeholder='location'
+						onChange={(e) => setLocation(e.target.value)}
+					/>
+				</label>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default SearchParams;
--- src/script.js
+++ src/script.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
+// import Pet from './Pet';
 
 const App = () => {
-	return React.createElement(
-		'div',
-		{},
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, {
-			name: 'Luna',
-			animal: 'Dog',
-			breed: 'Havanese'
-		}),
-		React.createElement(Pet, {
-			name: 'Vulcan',
-			animal: 'Dog',
-			breed: 'Labrator'
-		}),
-		React.createElement(Pet, {
-			name: 'Fepper',
-			animal: 'Bird',
-			breed: 'Cockatiel'
-		})
+	return (
+		<div>
+			<h1>Adopt Me!</h1>
+			<SearchParams />
+		</div>
 	);
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
