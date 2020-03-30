53a70857f21a406800f55bdbc48dddb486e52ae0
corozb <cristianorozco1984@gmail.com>
2020-03-30 11:25:56 -0500

    class components details & carousel

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -9,6 +9,7 @@ import React from 'react';
 // };
 
 import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
 
 class Details extends React.Component {
 	constructor(props) {
@@ -20,14 +21,14 @@ class Details extends React.Component {
 	}
 
 	componentDidMount() {
-		pet.animal(this.props.id).then(({ animal }) => {
+		pet.animal(+this.props.match.params.id).then(({ animal }) => {
 			this.setState({
 				name: animal.name,
 				animal: animal.type,
 				location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
 				description: animal.description,
 				media: animal.photos,
-				breed: animal.breed.primary,
+				breed: animal.breeds.primary,
 				loading: false
 			});
 		}, console.error);
@@ -38,9 +39,10 @@ class Details extends React.Component {
 			return <h1>loading...</h1>;
 		}
 
-		const { animal, breed, location, description, name } = this.state;
+		const { animal, breed, location, description, name, media } = this.state;
 		return (
 			<div className='details'>
+				<Carousel media={media} />
 				<div>
 					<h1>{name}</h1>
 					<h2>{`${animal} - ${breed} - ${location}`}</h2>
