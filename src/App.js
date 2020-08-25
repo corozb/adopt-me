6e828ce91f316387043f46e99aa38151fcb50a21
Cristian Orozco <cristianorozco@gmail.com>
2020-08-24 21:28:25 -0500

    custom hook

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SearchParams from './components/SearchParams/SearchParams'
+import './App.css'
+
+function App() {
+	return (
+		<div>
+			<h1 id='something-important'>Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	)
+}
+
+export default App
