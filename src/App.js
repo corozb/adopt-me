a62995c0f0231f0ab663d63b06165eeb3c98b8c6
Cristian Orozco <cristianorozco@gmail.com>
2020-08-25 14:43:41 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import SearchParams from './components/SearchParams/SearchParams'
+import Details from './components/Details/Details'
 import './App.css'
 
 function App() {
 	return (
 		<div>
-			<h1 id='something-important'>Adopt Me!</h1>
-			<SearchParams />
+			<Router>
+				<header>
+					<Link to='/'>
+						<h1>Adopt Me!</h1>
+					</Link>
+				</header>
+				<Route path='/' exact component={SearchParams} />
+				<Route path='/details/:id' exact component={Details} />
+			</Router>
 		</div>
 	)
 }
